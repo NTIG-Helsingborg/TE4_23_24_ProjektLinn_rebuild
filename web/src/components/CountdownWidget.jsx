128e97dd539561778540ec1706c303dd5774f50c
Jonte88 <jonathan.thj15@gmail.com>
2024-01-15 13:17:27 +0100

    Making footer in countdownWidget

--- web/src/components/CountdownWidget.jsx
+++ web/src/components/CountdownWidget.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import ntiLogo from "../assets/ntiLogo.svg";
 
-function CountdownWidget() {
+const size = "1x1";
+
+export const CountdownWidget = () => {
   return (
     <>
-      
+      <div className="countdownTopTextDiv">
+        <h1 className="countdownTopText">{}Jul-Lan</h1>
+      </div>
+      <div className="countdownCountdownDiv"></div>
+      <div className="countdownFooter bottom-0 flex justify-center width: 100%;">
+        <img src={ntiLogo} alt="NTI Gymnasiet" />
+      </div>
     </>
   );
-}
-
-export default CountdownWidget;
+};
