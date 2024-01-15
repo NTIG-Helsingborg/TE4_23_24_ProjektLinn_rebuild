a75b0f10099883aee431b480e01e9f67bc7bde64
Othmam-Al-Ani <142592326+Othmam-Al-Ani@users.noreply.github.com>
2024-01-15 13:25:19 +0100

    Create Insta widget component

---
+++ web/src/components/Insta.jsx
@@ -0,0 +1,21 @@
+import Iframe from 'react-iframe'
+
+
+export const Insta = () => {
+    return (
+        <div>
+            <p className='text-blue-500'> Jakob e cool </p>
+            
+            <Iframe url="https://www.instagram.com/p/C19EYB2M7cf/embed"
+               width="640px"
+               height="640px"
+               id=""
+               className=""
+               display="block"
+               position="relative"
+               scrolling='no'
+               />
+        </div>
+            )
+            }
+            
