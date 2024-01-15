676323936af088d5e91d255f7997198042f61b2b
WarMolee <oskar04@telia.com>
2024-01-15 11:38:09 +0100

    Added autoupdate for weatherWidget
    Made sure the widget updates its information but doesnt fetch every second.

--- web/src/components/weatherWidget.jsx
+++ web/src/components/weatherWidget.jsx
@@ -1,29 +1,22 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 
-const weatherWidget = () => {
-  const { isPending, error, data } = useQuery({
-    queryKey: ["weatherWidget"],
-    queryFn: () =>
-      fetch(
-        "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/12.70030/lat/56.0421/data.json" //      http://opendata.smhi.se/apidocs/metfcst/index.html - This is the API documentation
-      ).then((res) => res.json()),
-  });
+const weatherFetcher = async () => {
+  const res = await fetch(
+    "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/12.70030/lat/56.0421/data.json"
+  );
+  const data = await res.json();
 
-  if (isPending) return "Loading...";
-
-  if (error) return "An error has occurred: " + error.message;
-
-
-  
   // Get current time and split its part into an array
   const currentTimeFetch = new Date();
   const currentTimeSplit = currentTimeFetch.toString().split(" ");
 
   const currentTime = currentTimeSplit[4];
-  const closestHour = Math.round(parseInt(currentTime.split(":")[0]));
+  const closestHour = currentTime.split(":")[0].padStart(2, "0"); // Makes sure single digit hours are padded with a 0
   const currentMonth = currentTimeSplit[1];
+
   const getMonthNumber = (monthName) => {
+    // Translates the month name into a number
     const months = {
       Jan: "01",
       Feb: "02",
@@ -42,7 +35,6 @@ const weatherWidget = () => {
     return months[monthName];
   };
   const monthNumber = getMonthNumber(currentMonth);
-
   // Convert all the values into a string with the API format
   const searchTime =
     currentTimeSplit[3] +
@@ -54,7 +46,7 @@ const weatherWidget = () => {
     closestHour +
     ":00:00Z";
 
-  // Compare searthTime to the API's timeSeries array and find the matching index to see exactly what data is available for the current time
+  // Compare searchTime to the API's timeSeries array and find the matching index to see exactly what data is available for the current time
   let matchingIndex = -1;
 
   data.timeSeries.forEach((timeSeries, index) => {
@@ -65,27 +57,39 @@ const weatherWidget = () => {
   });
 
   if (matchingIndex === -1) {
-    return <p>ERR: No matching data found for searchTime: {searchTime}</p>;
+    throw new Error(`No matching data found for searchTime: ${searchTime}`);
   }
+
+  return {
+    currentTime,
+    associatedTime: data.timeSeries[matchingIndex].validTime.slice(11, 16),
+    weatherType: data.timeSeries[matchingIndex].parameters[18].values[0],
+    temperature: data.timeSeries[matchingIndex].parameters[10].values[0],
+    windSpeed: data.timeSeries[matchingIndex].parameters[14].values[0],
+    rainAmount: data.timeSeries[matchingIndex].parameters[5].values[0],
+  };
+};
+
+export const WeatherWidget = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["weatherWidget"],
+    refetchInterval: 3600 * 1000,
+    queryFn: weatherFetcher,
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
 
   return (
     <div>
-      <p>{currentTime} - Current Time</p>
-      <p>
-        {data.timeSeries[matchingIndex].validTime.slice(11, 16)} - Associated
-        time
-      </p>
+      <p>{data.currentTime} - Current Time</p>
+      <p>{data.associatedTime} - Associated time</p>
       <br />
-      <p>
-        {data.timeSeries[matchingIndex].parameters[18].values[0]} - WeatherType
-      </p>
-      <p>{data.timeSeries[matchingIndex].parameters[10].values[0]} °C</p>
-      <p>{data.timeSeries[matchingIndex].parameters[14].values[0]} m/s</p>
-      <p>
-        {data.timeSeries[matchingIndex].parameters[5].values[0]} mm/h - Average
-        rain amount
-      </p>
+      <p>{data.weatherType} - Weather Type</p>
+      <p>{data.temperature} °C</p>
+      <p>{data.windSpeed} m/s</p>
+      <p>{data.rainAmount} mm/h - Average rain amount</p>
     </div>
   );
 };
-export default weatherWidget;
