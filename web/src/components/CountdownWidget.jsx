5643ae8f6b5761c0bc13e6a5e37ceb7990a3735d
Jonte88 <jonathan.thj15@gmail.com>
2024-01-16 13:26:54 +0100

    2x1 is finished. Waiting for backend 	modified:   web/src/components/CountdownWidget.jsx

--- web/src/components/CountdownWidget.jsx
+++ web/src/components/CountdownWidget.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ntiLogo from "../assets/ntiLogo.svg";
 
-const size = "1x1";
+const size = "2x1";
 
 export const CountdownWidget = () => {
   if (size === "1x1") {
@@ -22,15 +22,14 @@ export const CountdownWidget = () => {
         </div>
       </>
     );
-  }
-  else if (size === "1x2") {
+  } else if (size === "1x2") {
     return (
       <>
         <div className="countdownDiv aspect-[1/2] flex flex-col">
           <div className="countdownTopTextDiv flex justify-center mt-10">
             <h1 className="countdownTopText text-center text-8xl">Jul-Lan</h1>
           </div>
-          <div className="countdownCountdownDiv flex-grow flex-col flex">
+          <div className="countdownCountdownDiv flex-grow flex-col flex justify-center">
             <table className="mt-40">
               <tr>
                 <td>
@@ -73,25 +72,44 @@ export const CountdownWidget = () => {
                 </td>
               </tr>
             </table>
-            {/* <div className="flex flex-row w-full items-center mt-40">
-              <h3 className="text-[9rem] ml-[17%] font-bold">160</h3>
-              <p className="text-6xl ml-10">Dagar</p>
-            </div>
-            <div className="flex flex-row w-full items-center">
-              <h3 className="text-[9rem] ml-[17%] font-bold">15</h3>
-              <p className="text-6xl ml-10">Timmar</p>
-            </div>
-            <div className="flex flex-row w-full items-center">
-              <h3 className="text-[9rem] ml-[17%] font-bold">05</h3>
-              <p className="text-6xl ml-10">Minuter</p>
-            </div>
-            <div className="flex flex-row w-full items-center">
-              <h3 className="text-[9rem] ml-[17%] font-bold">38</h3>
-              <p className="text-6xl ml-10">Sekunder</p>
-            </div> */}
           </div>
           <div className="countdownFooter mb-10">
             <img src={ntiLogo} alt="NTI Gymnasiet" className="mx-auto w-8/12" />
+          </div>
+        </div>
+      </>
+    );
+  } else if (size === "2x1") {
+    return (
+      <>
+        <div className="countdownDiv aspect-[2/1] flex flex-col">
+          <div className="countdownTopTextDiv flex justify-center mt-10">
+            <h1 className="countdownTopText text-center text-8xl">Jul-Lan</h1>
+          </div>
+          <div className="countdownCountdownDiv flex-grow flex-col flex justify-center text-center">
+            <table className="table-fixed ml-[10%] mr-[10%]">
+              <tr>
+                <td className="w-3/12">
+                  <h3 className="text-9xl font-semibold">160</h3>
+                  <p className="text-2xl">Dagar</p>
+                </td>
+                <td className="w-3/12">
+                  <h3 className="text-9xl font-semibold">15</h3>
+                  <p className="text-2xl">Timmar</p>
+                </td>
+                <td className="w-3/12">
+                  <h3 className="text-9xl font-semibold">05</h3>
+                  <p className="text-2xl">Minuter</p>
+                </td>
+                <td className="w-3/12">
+                  <h3 className="text-9xl font-semibold">30</h3>
+                  <p className="text-2xl">Sekunder</p>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div className="countdownFooter mb-10">
+            <img src={ntiLogo} alt="NTI Gymnasiet" className="mx-auto w-4/12" />
           </div>
         </div>
       </>
