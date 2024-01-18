35be95af23779612a4aa7a32eedd7907d1839710
Othmam-Al-Ani <142592326+Othmam-Al-Ani@users.noreply.github.com>
2024-01-18 13:48:14 +0100

    Create TrafficWidget responsive

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,12 +1,30 @@
+import { config } from 'localforage';
+import TrafficWidget from '../components/trafficWidget'
+
+import { useEffect } from 'react';
+
 import { CountdownWidget } from "../components/CountdownWidget";
 
 export const RootRoute = () => {
+  
+   
     return (
-        <>
-            <h1 className='text-green-500'>RootRoute</h1>
-        </>
-    )
-}
+        
+            <>
+                <h1 className='text-green-500'>RootRoute</h1>
+    
+                <div  className='m-10 aspect-[1/2] w-2/6 bg-transform' >
+                    <TrafficWidget divId={1}/>
+                </div>
+                <div className='m-10 aspect-[2/1] w-4/6 bg-transform' >
+                    <TrafficWidget divId={2}/>
+                </div>
+                <div  className='m-10 aspect-[1/1] w-2/6 bg-transform' >
+                    <TrafficWidget divId={3}/>
+                </div>
+            </>
+    );
+};
 
 /**
  * @type {import('react-router-dom').RouteObject}
