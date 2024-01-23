c546f84c883f575231413ba03c302dce51268d2a
Othmam-Al-Ani <142592326+Othmam-Al-Ani@users.noreply.github.com>
2024-01-23 10:49:50 +0100

    Ready for departure

--- web/src/components/Insta.jsx
+++ web/src/components/Insta.jsx
@@ -3,19 +3,20 @@ import Iframe from 'react-iframe'
 
 export const Insta = () => {
     return (
-        <div>
-            <p className='text-blue-500'> Jakob e cool </p>
+   
+           //Iframe to show instagram profile
+        // not completelty tested BE CAREFUL MIGHT EXPLODE
             
-            <Iframe url="https://www.instagram.com/p/C19EYB2M7cf/embed"
-               width="640px"
-               height="640px"
+            <Iframe url="https://www.instagram.com/ntihelsingborg//embed"
+               width="100%"
+               height="100%"
                id=""
                className=""
                display="block"
                position="relative"
                scrolling='no'
                />
-        </div>
+      
             )
             }
             
