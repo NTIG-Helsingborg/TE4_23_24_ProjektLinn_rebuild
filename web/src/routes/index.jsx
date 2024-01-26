72a8e11fb63aaa606dc5735b890f6236dcab93e5
Eduard4002 <54219870+Eduard4002@users.noreply.github.com>
2024-01-26 12:40:32 +0100

    Update index.jsx

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -8,29 +8,11 @@ import { CountdownWidget } from "../components/CountdownWidget";
 import { useMisc } from "../lib/hooks/useMisc";
 
 export const RootRoute = () => {
-  const dataForTrafficWidget = {
-    divId: "someId",
-    interval: 30000, // Custom interval in milliseconds
-    // Add any other properties needed by the TrafficWidget component
-  };
-  const dataTime = {
-    datetime: "2015-03-25",
-  };
+ 
 
-  /*
-    tidigare kod:
-    <DisplayContainer />
-    <Veckoevents />
-    <CountdownWidget data={dataTime} />
-    */
   return (
     <>
-      <TrafficWidget data={dataForTrafficWidget} />
-      <SlideTimer />
-      <NewsArticleWidget />
-      <Insta />
-      <CountdownWidget data={dataTime} />
-      {console.log(useMisc("st-hbg-c-departures"))}
+     <DisplayContainer />
     </>
   );
 };
