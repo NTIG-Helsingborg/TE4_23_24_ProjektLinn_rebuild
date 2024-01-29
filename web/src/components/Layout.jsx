4be2e2f08ce6ac54e6f2f2764c9d2b1bbc791271
Mizfra <vipkotenok939@gmail.com>
2024-01-29 10:22:01 +0100

    muy bien

--- web/src/components/Layout.jsx
+++ web/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 export const Layout = () => {
     return (
         <div className="grid grid-cols-2 grid-rows-3 gap-2 mx-8">
-            <div className="row-span-2 flex justify-center items-center rounded-md bg-slate-300 aspect-[1/2 + 2rem] ">
+            <div className="row-span-2 flex justify-center items-center rounded-md bg-slate-300 aspect-[1/2] my-1 ">
                 <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">+</button>
             </div>
             <div className="col-span-2 flex justify-center items-center rounded-md col-start-1 row-start-3 bg-slate-300 aspect-[2/1]  ">
