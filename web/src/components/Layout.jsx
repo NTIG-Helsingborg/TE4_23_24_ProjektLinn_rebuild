11556269b9fca980645ddae7a6d2db4a9323b40d
Mizfra <vipkotenok939@gmail.com>
2024-01-30 09:00:05 +0100

    duration css

--- web/src/components/Layout.jsx
+++ web/src/components/Layout.jsx
@@ -13,22 +13,22 @@ export const Layout = ({ updateFilterWidget,divImages }) => {
             <div className="row-span-2 flex justify-center items-center rounded-md bg-slate-300 aspect-[1/2] my-1 relative">
                 {divImages.div1 && <img src={divImages.div1} alt="widget" className="w-full h-auto object-contain cursor-pointer absolute top-0 left-0" />}
 
-                <button onClick={() => handleClick('1x2','div1')} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded z-10" >+</button>
+                <button onClick={() => handleClick('1x2','div1')} className="bg-blue-500 hover:bg-blue-400 duration-100 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded z-10" >+</button>
             </div>
             <div className="col-span-2 flex justify-center items-center rounded-md col-start-1 row-start-3 bg-slate-300 aspect-[2/1] relative">
                 {divImages.div2 && <img src={divImages.div2} alt="widget" className="w-full h-auto object-contain cursor-pointer absolute top-0 left-0" />}
 
-                <button onClick={() => handleClick('2x1','div2')} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded z-10" >+</button>
+                <button onClick={() => handleClick('2x1','div2')} className="bg-blue-500 hover:bg-blue-400 duration-100 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded z-10" >+</button>
             </div>
             <div className="col-start-2 flex justify-center items-center row-start-1 rounded-md bg-slate-300 aspect-w-1 aspect-h-1 aspect-square relative">
                 {divImages.div3 && <img src={divImages.div3} alt="widget" className="w-full h-auto object-contain cursor-pointer absolute top-0 left-0" />}
 
-                <button onClick={() => handleClick('1x1','div3')} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded z-10" >+</button>
+                <button onClick={() => handleClick('1x1','div3')} className="bg-blue-500 hover:bg-blue-400 duration-100 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded z-10" >+</button>
             </div>
             <div className="flex justify-center items-center rounded-md bg-slate-300 aspect-w-1 aspect-h-1 aspect-square relative">
                 {divImages.div4 && <img src={divImages.div4} alt="widget" className="w-full h-auto object-contain cursor-pointer absolute top-0 left-0" />}
 
-                <button onClick={() => handleClick('1x1','div4')} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded z-10" >+</button>
+                <button onClick={() => handleClick('1x1','div4')} className="bg-blue-500 hover:bg-blue-400 duration-100 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded z-10" >+</button>
             </div>
         </div>
     );
